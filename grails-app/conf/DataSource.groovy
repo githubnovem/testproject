071c1b76ff6ada07d4a267e36aaccefe6998bdbd
JINKY-PC\JINKY <kst.khornsothea@gmail.com>
2016-11-24 13:24:54 +0700

    update mysql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -4,12 +4,16 @@ dataSource {
 //    driverClassName = "org.h2.Driver"
 //    username = "sa"
 //    password = ""
+//	pooled = true
+//	jmxExport = true
+//	driverClassName =  "com.mysql.jdbc.Driver"
+//	//dialect =  org.hibernate.dialect.MySQL5InnoDBDialect
+//	username = "root"
+//	password = ""
 	pooled = true
 	jmxExport = true
-	driverClassName =  "com.mysql.jdbc.Driver"
-	//dialect =  org.hibernate.dialect.MySQL5InnoDBDialect
-	username = "root"
-	password = ""
+	driverClassName = "com.mysql.jdbc.Driver"
+	logSql = true
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -24,14 +28,20 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+//            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
 			//url: "jdbc:mysql://localhost:3306/test"
-			pooled = true
-			driverClassName = "com.mysql.jdbc.Driver"
+//			pooled = true
+//			driverClassName = "com.mysql.jdbc.Driver"
+//			username = "root"
+//			password = ""
+//			url = "jdbc:mysql://localhost:3306/test"
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/test?useUnicode=yes&characterEncoding=UTF-8"
+			dialect = "com.domain.mysql.dialect.MySQLUTF8InnoDBDialect"
 			username = "root"
 			password = ""
-			url = "jdbc:mysql://localhost:3306/test"
+			logSql = true
         }
     }
     test {
@@ -50,12 +60,43 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-			pooled = true
-			driverClassName = "com.mysql.jdbc.Driver"
+			
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/test?useUnicode=yes&characterEncoding=UTF-8"
+			dialect = "com.domain.mysql.dialect.MySQLUTF8InnoDBDialect"
 			username = "root"
 			password = ""
-			url = "jdbc:mysql://localhost:3306/test"
+			logSql = true
+			
+//            dbCreate = "update"
+//			pooled = true
+//			driverClassName = "com.mysql.jdbc.Driver"
+//			username = "root"
+//			password = ""
+//			url = "jdbc:mysql://localhost:3306/test"
+
+//			url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			//url: "jdbc:mysql://localhost:3306/test"
+//            properties {
+//               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+//               jmxEnabled = true
+//               initialSize = 5
+//               maxActive = 50
+//               minIdle = 5
+//               maxIdle = 25
+//               maxWait = 10000
+//               maxAge = 10 * 60000
+//               timeBetweenEvictionRunsMillis = 5000
+//               minEvictableIdleTimeMillis = 60000
+//               validationQuery = "SELECT 1"
+//               validationQueryTimeout = 3
+//               validationInterval = 15000
+//               testOnBorrow = true
+//               testWhileIdle = true
+//               testOnReturn = false
+//               jdbcInterceptors = "ConnectionState"
+//               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+//            }
         }
     }
 }
